a6238b68604d53da01f1dc3796c6d9b10de18ff0
Matt Rintoul <matt@sayyeah.com>
2024-11-13 15:16:26 -0500

    add chessboard to index route

--- app/components/main/Main.tsx
+++ app/components/main/Main.tsx
@@ -2,13 +2,15 @@ import { Slot } from "~/components/slot/Slot";
 
 export function Main({
   children,
+  asChild = false,
   className = "flex flex-col flex-1 items-center",
 }: {
   children: React.ReactNode;
+  asChild?: boolean;
   className?: string;
 }) {
   return (
-    <Slot fallbackAs="main" id="main" className={className}>
+    <Slot fallbackAs="main" id="main" className={className} asChild={asChild}>
       {children}
     </Slot>
   );
--- app/routes/__site/_index/_route.index.tsx
+++ app/routes/__site/_index/_route.index.tsx
@@ -6,39 +6,41 @@ import { TOPICS } from "~/data/data";
 
 export default function IndexRoute() {
   return (
-    <Main>
-      <div className="w-full max-w-screen-xl px-8 py-16 gap-10 flex flex-col">
-        <div className="w-full flex flex-col items-center justify-center gap-6">
-          <span className="flex gap-4 items-center">
-            <Icon name="sequence-logo" className="size-20" />
-            <h1 className="text-48 font-bold">Sequence Playbook</h1>
-          </span>
-          <span>
-            Mini tutorials on how to use{" "}
-            <a
-              href="/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline"
-            >
-              Sequence
-            </a>
-          </span>
-        </div>
+    <Main asChild>
+      <main style={{ backgroundImage: "url('/bg-chessboard.svg')" }}>
+        <div className="w-full max-w-screen-xl px-8 py-16 gap-10 flex flex-col">
+          <div className="w-full flex flex-col items-center justify-center gap-6">
+            <span className="flex gap-4 items-center">
+              <Icon name="sequence-logo" className="size-20" />
+              <h1 className="text-48 font-bold">Sequence Playbook</h1>
+            </span>
+            <span>
+              Mini tutorials on how to use{" "}
+              <a
+                href="/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline"
+              >
+                Sequence
+              </a>
+            </span>
+          </div>
 
-        <div className="grid md:grid-cols-3 gap-3">
-          {TOPICS.map((topic) => (
-            <TopicCard
-              key={topic.path}
-              title={topic.title}
-              path={topic.path}
-              icon={topic.icon}
-              description={topic.description}
-              theme={topic.theme}
-            />
-          ))}
+          <div className="grid md:grid-cols-3 gap-3">
+            {TOPICS.map((topic) => (
+              <TopicCard
+                key={topic.path}
+                title={topic.title}
+                path={topic.path}
+                icon={topic.icon}
+                description={topic.description}
+                theme={topic.theme}
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      </main>
     </Main>
   );
 }
