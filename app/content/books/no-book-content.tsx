d0d735d57702925c1d47854ead14ca893cdb7c02
Matt Rintoul <matt@sayyeah.com>
2024-11-18 14:16:21 -0500

    Update empty book

--- app/content/books/no-book-content.tsx
+++ app/content/books/no-book-content.tsx
@@ -2,7 +2,11 @@ export function NoBookContent({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
-      <div>No book content just yet</div>
+      <div className="px-4">
+        <div className="w-full aspect-video flex items-center justify-center bg-neutral-800 rounded-[9px]">
+          No book content just yet
+        </div>
+      </div>
     </>
   );
 }
