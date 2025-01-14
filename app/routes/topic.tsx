f0116b2df35895ebaa77761380000faf2db0002b
Matt Rintoul <matt@sayyeah.com>
2025-01-14 15:24:43 -0500

    Adjust card size to fix overlap on tablet screen sizes

--- app/routes/topic.tsx
+++ app/routes/topic.tsx
@@ -43,14 +43,7 @@ export default function TopicRoute() {
 
   return (
     <Main>
-      <div className="absolute top-0 right-0 w-[600px] h-[600px] opacity-35">
-        <Mask.BoxFade>
-          {/* <Image name="home" className="object-cover" /> */}
-        </Mask.BoxFade>
-      </div>
       <div className="flex flex-col gap-2 mb-12">
-        {/* <div className="hidden md:block absolute inset-0 w-full h-full z-0 gradient-mask border rounded-t-[12px] border-white/10 bg-white/5"></div> */}
-
         <Image name={icon} width={48} />
         <h1 className="text-32 sm:text-40 font-bold sm:leading-[1.05em] leading-tight">
           {title}
@@ -58,14 +51,14 @@ export default function TopicRoute() {
         <p>{description}</p>
       </div>
 
-      <div className="grid grid-cols-1 mx-auto sm:mx-none sm:max-w-none sm:grid-cols-2 md:grid-cols-2 md:gap-8 gap-4 items-stretch w-full">
+      <div className="grid grid-cols-1 mx-auto sm:mx-none sm:max-w-none md:grid-cols-2 xl:grid-cols-3 gap-4  items-stretch w-full">
         {books.map((book) => (
           <Fragment key={book.path}>
             <BackgroundImage name={book?.image?.src} />
             <InheritLinkFromChild asChild>
               <div
                 data-background={book?.image?.src}
-                className="aspect-video rounded-[0.75rem] relative flex flex-col justify-end overflow-clip bg-cover bg-center bg-no-repeat hover:-translate-y-1 hover:scale-[1.01] transition-transform duration-300 group"
+                className="aspect-video rounded-[0.75rem] max-w-full relative flex flex-col justify-end overflow-clip bg-cover bg-center bg-no-repeat hover:-translate-y-1 hover:scale-[1.01] transition-transform duration-300 group"
               >
                 <div className="absolute size-full inset-0 border group-hover:border-white/30 border-white/10 transition-all duration-300 rounded-[0.75rem] pointer-events-none"></div>
                 <div className="flex flex-col px-8 p-8 bg-gradient-to-b from-0% to-40% from-deep-purple-900/0 to-deep-purple-900/95 gap-1">
