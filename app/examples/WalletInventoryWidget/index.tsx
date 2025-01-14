edb77dbf0f085567adbfc31005dc0d4d617a6c12
Matt Rintoul <matt@sayyeah.com>
2025-01-14 11:59:48 -0500

    Update snippets, fix vite version and dependencies

--- app/examples/WalletInventoryWidget/index.tsx
+++ app/examples/WalletInventoryWidget/index.tsx
@@ -1,7 +1,7 @@
 
-    import { snippet } from './WalletInventoryWidgetSnippet';
+    import { steps } from './WalletInventoryWidgetSnippet';
     import { codeString } from './WalletInventoryWidgetString';
     import { WalletInventoryWidget as example } from './WalletInventoryWidget';
 
-    export const WalletInventoryWidget = Object.assign(example, { Snippet:snippet, String:codeString });
+    export const WalletInventoryWidget = Object.assign(example, {  steps, String:codeString });
   
