2d93f6ecd415a26c5a807a4a380bd892626b4941
Matt Rintoul <matt@sayyeah.com>
2025-01-14 13:56:10 -0500

    Squashed commit of the following:
    commit ac99478d2667bc1be73a9d8d09d5ce96e86887d9
    Author: Tomasz Dysinski <dysinski.tomasz@gmail.com>
    Date:   Mon Jan 13 20:55:13 2025 -0800
    
        use inventory widget on contract inventory book
    
    commit b954de424846d80882485f8e5e8aad0cdd29b2b0
    Author: Tomasz Dysinski <dysinski.tomasz@gmail.com>
    Date:   Mon Jan 13 11:58:42 2025 -0800
    
        fix for kit-wallet on remix
    
    commit cc795b2fa99f42914a24993ba1aa2a21dba3fe73
    Author: Tomasz Dysinski <dysinski.tomasz@gmail.com>
    Date:   Mon Jan 13 08:57:33 2025 -0800
    
        adding kit-wallet, ssr problems
        rolled react back to 18
    
    commit a40a635911046a12afc64e7bec658e467ae209db
    Author: Tomasz Dysinski <dysinski.tomasz@gmail.com>
    Date:   Mon Jan 13 08:20:37 2025 -0800
    
        removed unused reset
    
    commit b3da50afb8fa0c8502564f589dece9c1f19d1b29
    Author: Tomasz Dysinski <dysinski.tomasz@gmail.com>
    Date:   Mon Jan 13 08:19:51 2025 -0800
    
        upgraded eslint from 8 to 9, new config file. added example widget and nodemon to rebuild examples.

--- app/examples/WalletInventoryWidget/index.tsx
+++ app/examples/WalletInventoryWidget/index.tsx
@@ -1,7 +1,8 @@
+import { steps } from "./WalletInventoryWidgetSnippet";
+import { codeString } from "./WalletInventoryWidgetString";
+import { WalletInventoryWidget as example } from "./WalletInventoryWidget";
 
-    import { steps } from './WalletInventoryWidgetSnippet';
-    import { codeString } from './WalletInventoryWidgetString';
-    import { WalletInventoryWidget as example } from './WalletInventoryWidget';
-
-    export const WalletInventoryWidget = Object.assign(example, {  steps, String:codeString });
-  
+export const WalletInventoryWidget = Object.assign(example, {
+  steps,
+  String: codeString,
+});
